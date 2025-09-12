fb32c190956f2df408046f591f70fd8d670282b4
kosala <kosalamaduwantha0@gmail.com>
2025-09-12 22:58:44 +0530

    Refactor CSV to Parquet conversion function to use dataset configuration and streamline HDFS upload process

--- src/ingestion/csv_ingestion.py
+++ src/ingestion/csv_ingestion.py
@@ -6,35 +6,36 @@ import json
 from src.config.config import HDFS_HOST, HDFS_PORT
 
 def csv_to_parquet_hdfs(
-        local_csv_path, 
-        columns_to_keep, 
-        parquet_file_name, 
-        hdfs_path, 
-        hdfs_host='localhost', 
-        hdfs_port=8020
+        dataset_config: dict,
+        hdfs_host=HDFS_HOST, 
+        hdfs_port=HDFS_PORT,
+        hdfs_path='/user/kosala/input',
     ):
     """Convert a local CSV file to Parquet format and upload it to HDFS.
     Args:
-        local_csv_path (str): Path to the local CSV file.
-        columns_to_keep (list): List of column names to retain in the Parquet file.
-        parquet_file_name (str): Name of the output Parquet file.
+        dataset_config (dict): Configuration dictionary containing dataset details.
         hdfs_path (str): HDFS directory path where the Parquet file will be stored.
         hdfs_host (str): Hostname of the HDFS namenode.
         hdfs_port (int): Port number of the HDFS namenode.
     """
-    table = pv.read_csv(local_csv_path)
-    
-    # filter columns defined in columns_to_keep in metadata
-    filtered_table = table.select(columns_to_keep)
-    
-    # convert to parquet in memory
-    sink = pa.BufferOutputStream()
-    pq.write_table(filtered_table, sink)
-    parquet_buffer = sink.getvalue()
+    for dataset in dataset_config:
+        source_dataset_config = dataset.get('source', {})
+        table = pv.read_csv(source_dataset_config.get('path'))
 
-    hdfs = pafs.HadoopFileSystem(host=hdfs_host, port=hdfs_port)
-    with hdfs.open_output_stream(hdfs_path + '/' + parquet_file_name) as out_stream:
-        out_stream.write(parquet_buffer.to_pybytes())
+        # filter columns defined in columns_to_keep in metadata
+        filtered_table = table.select([col.get('name') for col in \
+            source_dataset_config.get('features', [])])
+        
+        # convert to parquet in memory
+        sink = pa.BufferOutputStream()
+        pq.write_table(filtered_table, sink)
+        parquet_buffer = sink.getvalue()
+
+        hdfs = pafs.HadoopFileSystem(
+            host=hdfs_host, port=hdfs_port)
+        with hdfs.open_output_stream(hdfs_path + '/' + \
+            source_dataset_config.get('name') + '.parquet') as out_stream:
+            out_stream.write(parquet_buffer.to_pybytes())
 
 def invok_e_csv_to_parquet(ocs_name='ecommerce_transactions'):
     """Invoke the CSV to Parquet conversion with example parameters."""
@@ -44,23 +45,15 @@ def invok_e_csv_to_parquet(ocs_name='ecommerce_transactions'):
         metadata_config = json.load(f)
 
     # Extract relevant info from metadata
-    dataset = metadata_config.get('dataset_config', [{}])[0]
-    source = dataset.get('source', {})
-    destination = dataset.get('destination', {})
-
-    local_csv_path = dataset.get('source', {}).get('path')
-    columns_to_keep = [feature['name'] for feature in source.get('features', [])]
-    # Parquet file name: use destination name or source name + .parquet
-    parquet_file_name = f"{destination.get('name', source.get('name', 'output'))}.parquet"
+    dataset_config = metadata_config.get('dataset_config', [{}])
+    destination = dataset_config.get('destination', {})
     # HDFS path: use destination path
     hdfs_path = destination.get('path', '/user/kosala/input')
     hdfs_host = HDFS_HOST
     hdfs_port = HDFS_PORT
 
     csv_to_parquet_hdfs(
-        local_csv_path=local_csv_path,
-        columns_to_keep=columns_to_keep,
-        parquet_file_name=parquet_file_name,
+        dataset_config=dataset_config,
         hdfs_path=hdfs_path,
         hdfs_host=hdfs_host,
         hdfs_port=hdfs_port
